f237ac55299ac96db30fa390885c148780c5e2f0
Gwanho Kim <khkim6040@gmail.com>
2026-03-08 13:23:27 +0900

    feat: 사이드바 드래그 리사이즈 UI 적용
    고정 w-[420px] 대신 동적 width 사용, 드래그 핸들 divider 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import Editor from "@/components/editor/Editor";
 import Preview from "@/components/preview/Preview";
+import { useResizable } from "@/hooks/useResizable";
 
 export default function Home() {
+  const { width, handleMouseDown, handleDoubleClick } = useResizable();
+
   return (
     <div className="flex h-screen min-w-[1024px] bg-zinc-100">
-      <div className="w-[420px] shrink-0 border-r border-zinc-200 bg-zinc-50">
+      <div
+        className="shrink-0 border-r border-zinc-200 bg-zinc-50"
+        style={{ width }}
+      >
         <Editor />
+      </div>
+      <div
+        className="group relative w-1.5 shrink-0 cursor-col-resize hover:bg-blue-400/40 active:bg-blue-400/60 transition-colors"
+        onMouseDown={handleMouseDown}
+        onDoubleClick={handleDoubleClick}
+      >
+        <div className="absolute inset-y-0 -left-1 -right-1" />
       </div>
       <div className="flex-1">
         <Preview />
