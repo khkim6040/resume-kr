d6b5241daa0632c0e043d62946b03de5069e90be
Gwanho Kim <khkim6040@gmail.com>
2026-03-14 22:06:06 +0900

    fix: Chromium 바이너리 CDN URL을 공식 릴리즈 URL로 수정

--- src/lib/browser.ts
+++ src/lib/browser.ts
@@ -2,7 +2,7 @@ import { chromium, type Browser } from "playwright-core";
 import chromiumBinary from "@sparticuz/chromium-min";
 
 const CHROMIUM_PACK_URL =
-  "https://github.com/nichochar/chromium-on-vercel/releases/download/v143.0.0/chromium-v143.0.0-pack.tar";
+  "https://github.com/Sparticuz/chromium/releases/download/v143.0.4/chromium-v143.0.4-pack.x64.tar";
 
 let browserInstance: Browser | null = null;
 
